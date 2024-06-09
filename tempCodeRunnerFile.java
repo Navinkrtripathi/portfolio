62f045e8eb9ce0f3eb43345e658323566f547b62
Navinkrtripathi <144321147+Navinkrtripathi@users.noreply.github.com>
2024-06-09 15:25:20 +0530

    Add files via upload
    It's a practice-based project.
    The project is a personal portfolio website.

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class javaBasics {
+
+    public static void main(String args[]) {
+        int counter = 1;
+        while(counter <= 10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
